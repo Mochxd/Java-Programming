ddaa47577d94128eed1a9e280b1b6d991f9238b8
Mohamed Mostafa <122634626+Mochxd@users.noreply.github.com>
2024-01-21 14:21:17 +0200

    Add files via upload

---
+++ Chapter10/Cat.java
@@ -0,0 +1,11 @@
+package Chapter10;
+
+public class Cat extends Animal{
+    @Override
+    public void makeSound(){
+        System.out.println("Meow");
+    }
+    public void Scratch(){
+        System.out.println("I am a cat. I scratch things");
+    }
+}
